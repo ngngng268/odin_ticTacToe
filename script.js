de50f4af01c59a5ab448f7502addaa8f7e14fb09
ngngng268 <thugiang268@gmail.com>
2026-05-12 22:48:17 +0700

    added a draft of the function that will be call once a cell is clicked

---
+++ script.js
@@ -0,0 +1,106 @@
+function ticTacToe() {
+    let gamePlayed = 0;
+    let board = [];
+    let lastMark = "none";
+    let winner = "noone";
+    let cellMarked = 0;
+    for (i = 0; i <= 8; i++) {
+        board[i] = " ";
+    } //set all cell to "none"
+    let game = "over";
+    const boardDiv = document.querySelector(".board");
+    const cellNodeList = boardDiv.querySelectorAll("div.cell");
+    const cellArr = Array.from(cellNodeList);
+    console.log(cellArr);
+    /*for (i = 0; i <= 8; i++) {
+        cellArr[i].textContent = board[i]
+    }*/
+    function reset() {
+        game = "over";
+        for (i = 0; i <= 8; i++) {
+            board[i] = " ";
+        };
+        lastMark = "none";
+        let winner = "noone";
+        cellMarked = 0;
+        for (i = 0; i <= 8; i++) {
+            cellArr[i].textContent = board[i];
+        };
+    }; //reset function
+
+    function display(content) {
+        bannerDiv.textContent = content;
+    } //display content on taskbar
+    const player = function (name, mark) {
+        this.name = name
+        this.mark = mark
+        this.score = 0
+    };
+    const player1 = new player("First", "x");
+    const player2 = new player("Second", "O");// need new button to open new dialog to enter new player name
+    console.log(player1);
+    function cell(mark) {
+        target.textContent = mark;
+        lastMark = mark;
+        board[target.id] = mark
+    }
+    function clickOnCell() {
+        if (game === "over") {
+            game = "onGoing";
+        };
+        if ((lastMark === "none")
+            || (lastMark === player2.mark)) {
+            cell(player1.mark)
+        } else if (lastMark === player1.mark) {
+            cell(player2.mark)
+        }
+        cellMarked = cellMarked + 1
+        if ((lastMark !== "none") &&
+            (((board[0] === board[1])
+                && (board[1] === board[2])
+                && (board[2] === lastMark))
+                || ((board[0] === board[3])
+                    && (board[3] === board[6])
+                    && (board[6] === lastMark))
+                || ((board[0] === board[4])
+                    && (board[4] === board[8])
+                    && (board[8] === lastMark))
+                || ((board[1] === board[4])
+                    && (board[4] === board[7])
+                    && (board[7] === lastMark))
+                || ((board[2] === board[5])
+                    && (board[5] === board[8])
+                    && (board[8] === lastMark))
+                || ((board[3] === board[4])
+                    && (board[4] === board[5])
+                    && (board[5] === lastMark))
+                || ((board[6] === board[7])
+                    && (board[7] === board[8])
+                    && (board[8] === lastMark))
+                || ((board[2] === board[4])
+                    && (board[4] === board[6])
+                    && (board[6] === lastMark))
+            )
+        ) {
+            if (lastMark === player1.mark) {
+                winner = player1;
+                player1.score = player1.score + 3;
+            } else if (lastMark === player2.mark) {
+                winner = player2;
+                player2.score = player2.score + 3
+            };
+
+            display(`Congratulations!! ${winner.name} is the winner of this game!!!!!`);
+            gamePlayed = gamePlayed + 1;
+            reset();
+
+        } else if ((lastMark !== "none") && (cellMarked === 9)) {
+            player1.score = player1.score + 1;
+            player2.score = player2.score + 1;
+            display(`It was a draw. Play again?`);
+            reset();
+            gamePlayed = gamePlayed + 1;
+        }
+    }
+}
+ticTacToe()
